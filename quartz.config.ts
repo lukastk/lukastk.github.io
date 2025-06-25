55062934c61aa3db6fc50e080baef912b841302b
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-06-25 08:18:39 +0100

    added tinylytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "tinylytics",
+      siteId: "7sXEfQwaNhamz1CZXxtk",
     },
     locale: "en-US",
     baseUrl: "lukastk.dev",
